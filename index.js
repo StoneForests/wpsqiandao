572f4fd607bf695e66bc3d6c5b7ac581b6b8faaa
shilinlin <shilinlin@cmsr.chinamobile.com>
2020-07-07 15:18:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var daka = require('./lib/wps')
 
 exports.main_handler = async () => {
     sckey = '你的server酱的key（非必填）';
-    sid = '你的wps的sid，cookie中以V02开头的那个字符串';
-    wpsinviteid = '你的wps的用户id';
+    sid = '第一步中的wps_sid';
+    wpsinviteid = '第二步中的分享id';
     return daka(sid, wpsinviteid, sckey)
 };
